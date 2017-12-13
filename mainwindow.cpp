fd0020c73e250ca4b44c051b96d7f3953cb5c3e6
Unknown <coen_koomen@hotmail.com>
2017-12-13 14:21:22 +0100

    Added correct handling of profile flag

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,7 +6,7 @@
 #include <QPainter>
 #include <QtDebug>
 
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow), threadPool(8)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
